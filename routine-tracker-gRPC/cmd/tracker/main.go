ce5fd8c91e88ada7df8ab332604a2bc94d2aeb28
itsfarhan <itsmefarhan09@gmail.com>
2026-03-16 15:04:45 +0530

    updated main.go file
    Signed-off-by: itsfarhan <itsmefarhan09@gmail.com>

--- routine-tracker-gRPC/cmd/tracker/main.go
+++ routine-tracker-gRPC/cmd/tracker/main.go
@@ -1,20 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"os"
-	"routine-tracker/routine-tracker-gRPC/internal/server"
-	"routine-tracker/routine-tracker-gRPC/log"
+
+	"github.com/itsfarhan/routine-tracker/internal/repository"
+	"github.com/itsfarhan/routine-tracker/internal/server"
+	"github.com/itsfarhan/routine-tracker/log"
 )
 
 const port = 28710
 
 func main() {
 	lgr := log.New(os.Stdout)
-	srv := server.New(lgr)
-	err := srv.ListenAndServe(port)
-	if err != nil {
-		lgr.Logf("Error while running the server: %s", err)
+	repo := repository.New(lgr)  // create the in-memory store
+	srv := server.New(repo, lgr) // inject it into the server
+	if err := srv.ListenAndServe(port); err != nil {
+		lgr.Logf("server error: %s", err)
 		os.Exit(1)
 	}
 }
